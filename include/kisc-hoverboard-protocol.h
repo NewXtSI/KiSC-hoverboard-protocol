9394d9b1844d6162ed8766e849a9932983b12032
Henning Schreiber <henning.schreiber@wago.com>
2024-11-26 12:01:50 +0100

    Refactor hoverboard protocol definitions and add serial command structures

--- include/kisc-hoverboard-protocol.h
+++ include/kisc-hoverboard-protocol.h
@@ -2,110 +2,104 @@
 #define INCLUDE_KISC_HOVERBOARD_PROTOCOL_INCLUDED
 
 #include <stdint.h>
+#include <stdbool.h>
 
-namespace kisc {
-namespace protocol {
+#ifdef __cplusplus
+extern "C" {
+#endif
 
-enum class ControlType : uint8_t {
+typedef enum {
     Commutation,
     Sinusoidal,
     FieldOrientedControl
-};
+} ControlType;
 
-enum class ControlMode : uint8_t {
+typedef enum {
     OpenMode,
     Voltage,
     Speed,  // Only with FieldOrientedControl
     Torque  // Only with FieldOrientedControl
-};
+} ControlMode;
 
-}  // namespace protocol
-}  // namespace kisc
+typedef struct {
+    bool enable;
+    int16_t pwm;
+    ControlType ctrlTyp;
+    ControlMode ctrlMod;
+    uint8_t iMotMax;
+    uint8_t iDcMax;
+    uint16_t nMotMax;
+    uint8_t fieldWeakMax;
+    uint8_t phaseAdvMax;
+    bool cruiseCtrlEna;
+    int16_t nCruiseMotTgt;
+} MotorState;
 
-namespace kisc {
-namespace protocol {
-namespace serial {
-
-struct MotorState {
-    bool enable = false;
-    int16_t pwm = 0;
-    ControlType ctrlTyp = ControlType::FieldOrientedControl;
-    ControlMode ctrlMod = ControlMode::OpenMode;
-    uint8_t iMotMax = 15;                  // [A] Maximum motor current limit
-    uint8_t iDcMax = 17;                   // [A] Maximum DC Link current limit (This is the final current protection. Above this value, current chopping is applied. To avoid this make sure that I_DC_MAX = I_MOT_MAX + 2A)
-    uint16_t nMotMax = 1000;               // [rpm] Maximum motor speed limit
-    uint8_t fieldWeakMax = 10;             // [A] Maximum Field Weakening D axis current (only for FOC). Higher current results in higher maximum speed.
-    uint8_t phaseAdvMax = 40;              // [deg] Maximum Phase Advance angle (only for SIN). Higher angle results in higher maximum speed.
-    bool cruiseCtrlEna = false;
-    int16_t nCruiseMotTgt = 0;
-};
-
-inline uint16_t calculateChecksum(MotorState state) {
+static inline uint16_t calculateStateChecksum(MotorState state) {
     return
-        uint16_t(state.enable) ^
+        (uint16_t)state.enable ^
         state.pwm ^
-        uint16_t(state.ctrlTyp) ^
-        uint16_t(state.ctrlMod) ^
+        (uint16_t)state.ctrlTyp ^
+        (uint16_t)state.ctrlMod ^
         state.iMotMax ^
         state.iDcMax ^
         state.nMotMax ^
         state.fieldWeakMax ^
         state.phaseAdvMax ^
-        uint16_t(state.cruiseCtrlEna) ^
+        (uint16_t)state.cruiseCtrlEna ^
         state.nCruiseMotTgt;
 }
 
-struct BuzzerState {
-    uint8_t freq = 0;
-    uint8_t pattern = 0;
-};
+typedef struct {
+    uint8_t freq;
+    uint8_t pattern;
+} BuzzerState;
 
-inline uint16_t calculateChecksum(BuzzerState state) {
+static inline uint16_t calculateBuzzerChecksum(BuzzerState state) {
     return state.freq ^ state.pattern;
 }
 
-struct Command {
-    static constexpr uint16_t VALID_HEADER = 0xAAAA;
-    static constexpr uint16_t INVALID_HEADER = 0xFFFF;
+typedef struct {
+    uint16_t        start;
+    MotorState      left, right;
+    BuzzerState     buzzer;
+    bool            poweroff;
+    bool            led;
+    bool            cruiseCtrlAcv;
+    bool            standstillAcv;
+    uint8_t         electricBrakeAmount;
+    uint16_t        checksum;
+} SerialCommand;
 
-    uint16_t start;
-
-    MotorState left, right;
-
-    BuzzerState buzzer;
-
-    bool poweroff = false;
-    bool led = false;
-
-    uint16_t checksum;
-};
-
-inline uint16_t calculateChecksum(Command command) {
+static inline uint16_t calculateCommandChecksum(SerialCommand command) {
     return command.start ^
-           calculateChecksum(command.left) ^
-           calculateChecksum(command.right) ^
-           calculateChecksum(command.buzzer) ^
+           calculateStateChecksum(command.left) ^
+           calculateStateChecksum(command.right) ^
+           calculateBuzzerChecksum(command.buzzer) ^
            command.poweroff ^
-           command.led;
+           command.led ^
+              command.cruiseCtrlAcv ^
+                command.standstillAcv ^
+                    command.electricBrakeAmount;
 }
 
-struct MotorFeedback {
-    int16_t   angle = 0;
-    int16_t   speed = 0;
-    uint8_t   error = 0;
-    int16_t   dcLink = 0;
-    int16_t   dcPhaA = 0;
-    int16_t   dcPhaB = 0;
-    int16_t   dcPhaC = 0;
-    uint16_t  chops = 0;
-    int16_t   id = 0;
-    int16_t   iq = 0;
-    bool      hallA = false;
-    bool      hallB = false;
-    bool      hallC = false;
-};
+typedef struct {
+    int16_t   angle;
+    int16_t   speed;
+    uint8_t   error;
+    int16_t   dcLink;
+    int16_t   dcPhaA;
+    int16_t   dcPhaB;
+    int16_t   dcPhaC;
+    uint16_t  chops;
+    int16_t   id;
+    int16_t   iq;
+    bool      hallA;
+    bool      hallB;
+    bool      hallC;
+} MotorFeedback;
 
-inline uint16_t calculateChecksum(MotorFeedback feedback) {
+static inline uint16_t calculateMotorFeedbackChecksum(MotorFeedback feedback) {
     return feedback.angle ^ feedback.speed ^
            feedback.error ^ feedback.dcLink ^
            feedback.dcPhaA ^ feedback.dcPhaB ^
@@ -113,32 +107,34 @@ inline uint16_t calculateChecksum(MotorFeedback feedback) {
            feedback.hallA ^ feedback.hallB ^ feedback.hallC;
 }
 
-struct Feedback {
-    static constexpr uint16_t VALID_HEADER = 0xAAAA;
-    static constexpr uint16_t INVALID_HEADER = 0xFFFF;
-
+typedef struct {
     uint16_t start;
+    MotorFeedback   left, right;
+    int16_t         batVoltage;
+    int16_t         boardTemp;
+    int16_t         timeoutCntSerial;
+    bool            cruiseCtrlAcv;
+    bool            standstillAcv;
+    uint8_t         electricBrakeAmount;
+    uint16_t        checksum;
+} SerialFeedback;
 
-    MotorFeedback left, right;
-
-    int16_t   batVoltage = 0;
-    int16_t   boardTemp = 0;
-
-    int16_t timeoutCntSerial = 0;
-
-    uint16_t checksum;
-};
-
-inline uint16_t calculateChecksum(Feedback feedback) {
+static inline uint16_t calculateFeedbackChecksum(SerialFeedback feedback) {
     return feedback.start ^
-           calculateChecksum(feedback.left) ^
-           calculateChecksum(feedback.right) ^
+           calculateMotorFeedbackChecksum(feedback.left) ^
+           calculateMotorFeedbackChecksum(feedback.right) ^
            feedback.batVoltage ^
            feedback.boardTemp ^
-           feedback.timeoutCntSerial;
+           feedback.timeoutCntSerial ^
+              feedback.cruiseCtrlAcv ^
+                feedback.standstillAcv ^
+                    feedback.electricBrakeAmount;
+                    
 }
+
+#ifdef __cplusplus
 }
-}
-}
+#endif
+
 
 #endif  /* INCLUDE_KISC_HOVERBOARD_PROTOCOL_INCLUDED */
