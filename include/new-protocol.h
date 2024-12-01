5e63a32a18b00908b67f13a7a4c4e71c97b1e0e1
Henning Schreiber <henning.schreiber@wago.com>
2024-12-01 07:59:22 +0100

    Add protocol versioning and expand command definitions with size specifications

--- include/new-protocol.h
+++ include/new-protocol.h
@@ -1,8 +1,13 @@
 #ifndef INCLUDE_NEW_PROTOCOL_INCLUDED
 #define INCLUDE_NEW_PROTOCOL_INCLUDED
 
-#define HOVER_VALID_HEADER  0xABCD
+#define HOVER_PROTOVERSION  0x00
+#define HOVER_VALID_HEADER  (0xABCD + HOVER_PROTOVERSION)
+
+#define HOVER_CMD_PING_SIZE         0
 #define HOVER_CMD_PING      0x01                // Data null        Size 5
+
+#define HOVER_CMD_STATUS_SIZE       7
 #define HOVER_CMD_STATUS    0x02                // Size 7
 // Data:
 //   Battery voltage (mV) 2 bytes
@@ -15,6 +20,7 @@
 //     Bit 3: Error Motor Left
 //     Bit 4: Error Motor Right
 //     Bit 5: Error Board
+#define HOVER_CMD_MOTORSTAT_SIZE    10
 #define HOVER_CMD_MOTORSTAT   0x03              // Size 11
 // Data:
 //   Speed Left (rpm) 2 bytes
@@ -24,9 +30,12 @@
 //   Flags Left 1 byte
 //     Bit 0: Motor Left enabled
 //     Bit 1: Motor Left error
+//     Bit 2: Motor Left cruiseControl
 //   Flags Right 1 byte
 //     Bit 0: Motor Right enabled
 //     Bit 1: Motor Right error
+//     Bit 2: Motor Right cruiseControl
+#define HOVER_CMD_MOTORCTRL_SIZE    6
 #define HOVER_CMD_MOTORCTRL   0x04              // Size 13
 // Data:
 //    Motor left target 2 bytes
@@ -36,7 +45,8 @@
 //      Bit 2: Control Type 0: Commutation, 1: Sinusoidal, 2: Field Oriented Control
 //      Bit 3: Control Type secondary
 //      Bit 4: Control Mode 0: Open, 1: Voltage, 2: Speed, 3: Torque
-//      Bit 5: Control Mode secondary	
+//      Bit 5: Control Mode secondary
+//      Bit 6: Cruise control enable
 //    Motor right target 2 bytes
 //    Motor right flags 1 byte
 //      Bit 0: Enable
@@ -45,14 +55,18 @@
 //      Bit 3: Control Type secondary
 //      Bit 4: Control Mode 0: Open, 1: Voltage, 2: Speed, 3: Torque
 //      Bit 5: Control Mode secondary
-#define HOVER_CMD_BUZZER      0x05              
+//      Bit 6: Cruise control enable
+#define HOVER_CMD_BUZZER_SIZE    2
+#define HOVER_CMD_BUZZER      0x05
 // Data:
 //    Frequency 1 byte
 //    Pattern 1 byte
+#define HOVER_CMD_POWER_SIZE    1
 #define HOVER_CMD_POWER       0x06              // Size 1
 // Data:
 //    Power off 1 byte
-#define HOVER_CMD_SETTINGS    0x07             
+#define HOVER_CMD_SETTINGS_SIZE 5
+#define HOVER_CMD_SETTINGS    0x07
 // Data:
 //    Max current 2 bytes
 //    Max speed 2 bytes
